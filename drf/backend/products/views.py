daffd7179fa6305ab192b4296eac918b6255ce31
beba2123 <antenehbeba@gmail.com>
2024-02-27 16:09:24 +0300

    some updates on my rest_framework  api   project

--- drf/backend/products/views.py
+++ drf/backend/products/views.py
@@ -1,15 +1,19 @@
-from rest_framework import generics
+from rest_framework import generics, mixins, permissions, authentication
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
+from .permissions import IsStaffEditorPermision
 from .serializers import ProductSerializer
 from django.shortcuts import get_object_or_404
 
 class ProductListCreateApiView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = [authentication.SessionAuthentication,
+                                    authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAdminUser, IsStaffEditorPermision]
 
-    def perform_create(self, serialzer):
+    def perform_create(self, serializer):
         print(serializer)
         serializer.save()
 
@@ -36,6 +40,37 @@ class ProductDestroyApiView(generics.DestroyAPIView):
 
     def perform_Delete(self, instance):
         super().perform_Delete(instance)
+
+
+class ProductMixinsView(mixins.CreateModelMixin,
+                        mixins.ListModelMixin,
+                        mixins.RetrieveModelMixin,
+                        generics.GenericAPIView):
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+    def get(self, request, *args, **kwargs):
+        print(args, kwargs)
+        pk = kwargs.get(self.lookup_field)
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        else:
+            return self.list(request, *args, **kwargs)
+
+            # return self.list(request, *args, **kwargs)
+    def post(self,  request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        print(serializer)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content')
+        if content is None:
+            content =  f"This product was created by {title}"
+        serializer.save(content= content)
+
+
 
 @api_view(['GET', 'POST'])
 def product_alt_view(request, pk=None, *args, **kwargs):
--- drf/py_client/List.py
+++ drf/py_client/List.py
@@ -1,7 +1,18 @@
 import requests
+from getpass import getpass
+auth_endpoint = "http://127.0.0.1:8000/api/auth/"
 
-endpoint = "http://127.0.0.1:8000/api/products/"
+password = getpass()
+auth_response = requests.post(auth_endpoint, json={'username': 'beba', 'password':password})
+print(auth_response.json())
+
+if  auth_response.status_code == 200:
+     token = auth_response.json().get('token')
+     header = {
+          'Authorization': f'Token {token}'
+     }
+     endpoint = "http://127.0.0.1:8000/api/products/"
 
 
-get_response = requests.get(endpoint)
-print(get_response.json())
+     get_response = requests.get(endpoint, headers=header)
+     print(get_response.json())
